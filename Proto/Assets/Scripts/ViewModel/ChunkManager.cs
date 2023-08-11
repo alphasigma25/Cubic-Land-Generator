09718cfd1b378115206a7ff9f8fd559b70a22269
fg1989 <fgazzett@gmail.com>
2023-08-11 17:07:11 +0200

    Simplification du chunk manager

--- Proto/Assets/Scripts/ViewModel/ChunkManager.cs
+++ Proto/Assets/Scripts/ViewModel/ChunkManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Math = System.Math;
 using MathF = System.MathF;
@@ -95,15 +96,8 @@ internal class ChunkManager
         {
             for (int k = 0; k < IGenerator.ZONE_SIZE; k++)
             {
-                BlocType type = zone[0, j, k];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(decalX, j + decalY, k + decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
-                }
+                GenerateBloc(list, zone, 0, j, k, decalX, decalY, decalZ);
+                GenerateBloc(list, zone, maxMoins, j, k, decalX, decalY, decalZ);
             }
         }
 
@@ -111,89 +105,42 @@ internal class ChunkManager
         {
             for (int k = 0; k < IGenerator.ZONE_SIZE; k++)
             {
-                BlocType type = zone[i, 0, k];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(i + decalX, decalY, k + decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
-                }
+                GenerateBloc(list, zone, i, 0, k, decalX, decalY, decalZ);
+                GenerateBloc(list, zone, i, maxMoins, k, decalX, decalY, decalZ);
             }
 
             for (int j = 1; j < maxMoins; j++)
             {
-                BlocType type = zone[i, j, 0];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(i + decalX, j + decalY, decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
-                }
+                GenerateBloc(list, zone, i, j, 0, decalX, decalY, decalZ);
+                GenerateBloc(list, zone, i, j, maxMoins, decalX, decalY, decalZ);
 
                 for (int k = 1; k < maxMoins; k++)
                 {
-                    type = zone[i, j, k];
-
                     bool nearAir = zone[i + 1, j, k] == BlocType.Air || zone[i - 1, j, k] == BlocType.Air ||
                         zone[i, j + 1, k] == BlocType.Air || zone[i, j - 1, k] == BlocType.Air ||
                         zone[i, j, k + 1] == BlocType.Air || zone[i, j, k - 1] == BlocType.Air;
 
-                    if (nearAir && type != BlocType.Air)
-                    {
-                        GameObject newCube
-                            = Object.Instantiate(
-                                RessourcesManager.Cube, new Vector3(i + decalX, j + decalY, k + decalZ), Quaternion.identity);
-                        newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                        list.Add(newCube);
-                    }
-                }
-
-                type = zone[i, j, maxMoins];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(i + decalX, j + decalY, maxMoins + decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
-                }
-            }
-
-            for (int k = 0; k < IGenerator.ZONE_SIZE; k++)
-            {
-                BlocType type = zone[i, maxMoins, k];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(i + decalX, maxMoins + decalY, k + decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
-                }
-            }
-        }
-
-        for (int j = 0; j < IGenerator.ZONE_SIZE; j++)
-        {
-            for (int k = 0; k < IGenerator.ZONE_SIZE; k++)
-            {
-                BlocType type = zone[maxMoins, j, k];
-                if (type != BlocType.Air)
-                {
-                    GameObject newCube
-                        = Object.Instantiate(
-                            RessourcesManager.Cube, new Vector3(maxMoins + decalX, j + decalY, k + decalZ), Quaternion.identity);
-                    newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
-                    list.Add(newCube);
+                    if (nearAir)
+                        GenerateBloc(list, zone, i, j, k, decalX, decalY, decalZ);
                 }
             }
         }
 
         return list;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void GenerateBloc(List<GameObject> list, BlocType[,,] zone, int i, int j, int k, int decalX, int decalY, int decalZ)
+    {
+        BlocType type = zone[i, j, k];
+        if (type == BlocType.Air)
+            return;
+
+        GameObject newCube
+            = Object.Instantiate(
+                RessourcesManager.Cube, new Vector3(i + decalX, j + decalY, k + decalZ), Quaternion.identity);
+        newCube.GetComponent<MeshRenderer>().material = RessourcesManager.GetMaterial(type);
+        list.Add(newCube);
     }
 
     private Vector3Int currentPos = new(int.MinValue, int.MinValue, int.MinValue);
