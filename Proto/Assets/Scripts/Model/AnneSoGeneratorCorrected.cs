914ababeda90bfa41fe72db52abbcad0f56e9fa9
fg1989 <fgazzett@gmail.com>
2023-08-11 05:46:02 +0200

    Utilisation du cache dans le generateur

--- Proto/Assets/Scripts/Model/AnneSoGeneratorCorrected.cs
+++ Proto/Assets/Scripts/Model/AnneSoGeneratorCorrected.cs
@@ -2,22 +2,31 @@
 
 internal class AnneSoGeneratorCorrected : IGenerator
 {
+    public AnneSoGeneratorCorrected()
+    {
+        heigthsCache = new((x, y) =>
+        {
+            // génération de la carte des hauteurs
+            int[,] heights = new int[IGenerator.ZONE_SIZE, IGenerator.ZONE_SIZE];
+            for (int i = 0; i < IGenerator.ZONE_SIZE; ++i)
+            {
+                for (int j = 0; j < IGenerator.ZONE_SIZE; ++j)
+                {
+                    heights[i, j]
+                        = (int)(IGenerator.ZONE_SIZE * Mathf.PerlinNoise(
+                            x + (i / IGenerator.ZONE_SIZE_FLOAT), y + (j / IGenerator.ZONE_SIZE_FLOAT)));
+                }
+            }
+            return heights;
+        });
+    }
+
     public BlocType[,,] Generate(Vector3Int zone)
     {
         BlocType[,,] result = new BlocType[IGenerator.ZONE_SIZE, IGenerator.ZONE_SIZE, IGenerator.ZONE_SIZE];
         const int MAX_DIRT_HEIGHT = 1;
 
-        // génération de la carte des hauteurs
-        int[,] heights = new int[IGenerator.ZONE_SIZE, IGenerator.ZONE_SIZE];
-        for (int i = 0; i < IGenerator.ZONE_SIZE; ++i)
-        {
-            for (int j = 0; j < IGenerator.ZONE_SIZE; ++j)
-            {
-                heights[i, j]
-                    = (int)(IGenerator.ZONE_SIZE * Mathf.PerlinNoise(
-                        zone.x + (i / IGenerator.ZONE_SIZE_FLOAT), zone.z + (j / IGenerator.ZONE_SIZE_FLOAT)));
-            }
-        }
+        int[,] heights = heigthsCache[zone.x, zone.z];
 
         for (int x = 0; x < IGenerator.ZONE_SIZE; ++x)
         {
@@ -39,4 +48,6 @@ internal class AnneSoGeneratorCorrected : IGenerator
 
         return result;
     }
+
+    private readonly BiCache<int[,]> heigthsCache;
 }
